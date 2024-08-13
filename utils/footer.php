95c77e9b9caf1efc7f1f05868706542f3aa5b90c
Nishanth1409 <nishanthkr1409@gmail.com>
2024-08-13 09:45:08 +0530

    updated

--- utils/footer.php
+++
@@ -1,7 +0,0 @@
-<hr class="footerline"><!--css modified horizontal line-->
-<footer>
-    <div class="container">
-        <h5 style="text-align: center;">&copy; 2024 BY: N K R</h5>
-        <br>
-    </div>
-</footer>
